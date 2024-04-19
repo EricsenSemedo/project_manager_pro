9aa1d2847ce7df89aebb0f54b358368d4f3bce64
EricsenSemedo <ericsen_semedo@uri.edu>
2024-04-19 18:10:10 -0400

    added boiler plate

--- project_events.php
+++ project_events.php
@@ -3,3 +3,26 @@
     Add user
     Remove user
 -->
+<?php
+
+?>
+
+<!DOCTYPE>
+<HTML>
+    <HEAD>
+        <TITLE>Project Events</TITLE>
+        <link rel="stylesheet" href="css/style.css">
+    </HEAD>
+    <BODY>
+        <div class="project">
+            <h1>Project Events</h1>
+            <form action="project_events.php" method="post">
+                <input type="text" name="event_name" placeholder="Event Name">
+                <input type="text" name="event_description" placeholder="Event Description">
+                <input type="date" name="event_date">
+                <input type="time" name="event_time">
+                <input type="submit" name="submit" value="Add Event">
+            </form>
+        </div>
+    </BODY>
+</HTML>
