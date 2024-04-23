cc155974fcca1c0af5c4b8c87e67b20c9bd51388
erichpriemer123 <90418024+erichpriemer123@users.noreply.github.com>
2024-04-23 12:59:44 -0400

    Added functionality to the form files. User can now add events sprints and tasks. When created user get rerouted baack to the project view where they can see the added t,s, or e

--- project_sprints_form.php
+++ project_sprints_form.php
@@ -11,6 +11,36 @@
     // Get the user_id from the session
     $user_id = $_SESSION["user_id"];
 
+    //get the project id 
+    $project_id = $_GET["id"];
+
+
+    // Check if the form has been submitted
+    if(!empty($_POST) && $_SERVER["REQUEST_METHOD"] == "POST"){
+        $sprint_title = $_POST["sprint_title"];
+        $sprint_description = $_POST["sprint_description"];
+        $sprint_start_dateTime = $_POST["sprint_start_dateTime"];
+        $sprint_end_dateTime = $_POST["sprint_end_dateTime"];
+
+        //check if any of the fields are empty 
+        if(empty($sprint_title) || empty($sprint_description) || empty($sprint_start_dateTime) || empty($sprint_end_dateTime)){
+            $errorMessage = "Please fill out all fields";
+            exit;
+        }
+
+        //sql statement insets into sprints
+        $sql = "INSERT INTO Sprint (project_id, sprint_title, sprint_description, start_date, end_date)
+            VALUES (:project_id, :sprint_title, :sprint_description, :start_date, :end_date);";
+
+        try{
+            pdo($pdo, $sql, ["project_id" => $project_id, "sprint_title" => $sprint_title, "sprint_description" => $sprint_description, "start_date" => $sprint_start_dateTime, "end_date" => $sprint_end_dateTime]);
+            header("Location: project_view.php?id=$project_id");
+            exit;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }    
+    }
+
 ?>
 
 <!DOCTYPE>
@@ -22,14 +52,14 @@
     <BODY>
         <div class="project">
             <h1>Add Sprint to Project</h1>
-            <form action="project_sprints_form.php" method="post">
+            <form action="project_Sprints_form.php?id=<?= $project_id ?>" method="post">
                 <input type="text" name="sprint_title" placeholder="Sprint Name">
                 <input type="text" name="sprint_description" placeholder="Sprint Description">
                 <label>Start Date</label>
-                <input type="datetime-local" name="sprint_startdatetime">
+                <input type="datetime-local" name="sprint_start_dateTime">
                 <label>End Date</label>
-                <input type="datetime-local" name="sprint_enddatetime">
-                <input type="submit" name="submit" value="Add Event">
+                <input type="datetime-local" name="sprint_end_dateTime">
+                <input type="submit" name="submit" value="Add Sprint">
             </form>
         </div>
     </BODY>
--- project_tasks_form.php
+++ project_tasks_form.php
@@ -11,6 +11,33 @@
     // Get the user_id from the session
     $user_id = $_SESSION["user_id"];
 
+    //get the project id 
+    $project_id = $_GET["id"];
+
+    if(!empty($_POST) && $_SERVER["REQUEST_METHOD"] == "POST"){
+        $task_title = $_POST["task_title"];
+        $task_description = $_POST["task_description"];
+        $task_status = $_POST["task_status"];
+
+        //check title,description,status are not empty
+        if(empty($task_title) || empty($task_description) || empty($task_status)){
+            $errorMessage = "Please fill out all fields";
+            exit;
+        }
+
+        //sql statement insets into task
+        $sql = "INSERT INTO Task (user_id,title, description, status, project_id)
+            VALUES (:user_id,:title, :description, :status, :project_id);";
+
+        try{
+            pdo($pdo, $sql, ["user_id" => $user_id,"title" => $task_title, "description" => $task_description, "status" => $task_status, "project_id" => $project_id]);
+            header("Location: project_view.php?id=$project_id");
+            exit;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
 ?>
 
 <!DOCTYPE>
@@ -22,7 +49,7 @@
     <BODY>
         <div class="project">
             <h1>Add task to Project</h1>
-            <form action="project_tasks_form.php" method="post">
+            <form action="project_tasks_form.php?id=<?= $project_id ?>" method="post">
                 <input type="text" name="task_title" placeholder="Task Name">
                 <input type="text" name="task_description" placeholder="Task Description">
                 <input type="text" name="task_status" placeholder="Task Status">
