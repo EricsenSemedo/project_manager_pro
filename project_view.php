df83aa7bfd6105d4bffe29c6725b536b2993e59c
EricsenSemedo <ericsen_semedo@uri.edu>
2024-04-22 23:23:34 -0400

    prepping for khan ban view

--- project_view.php
+++ project_view.php
@@ -23,7 +23,17 @@
             FROM Projects
             WHERE project_id = :project_id;";
         $project = pdo($pdo, $sql, ["project_id" => $projectId])->fetch();
+
         return $project;
+    }
+
+    function getAssociatedTasks(PDO $pdo, int $projectId){
+        $sql = "SELECT task_id, title, description, due_date
+            FROM Tasks
+            WHERE project_id = :project_id;";
+        $tasks = pdo($pdo, $sql, ["project_id" => $projectId])->fetchAll();
+
+        return $tasks;
     }
 
     $projectDetails = getProjectDetails($pdo, $projectId);
@@ -37,17 +47,29 @@
         <link rel="stylesheet" href="css/style.css">
     </HEAD>
     <BODY>
-        <h1><?= $projectDetails["title"] ?></h1>
-        <p><?= $projectDetails["description"] ?></p>
-        
-        <a href="project_tasks.php?id=<?= $projectId ?>"><h3>Tasks</h3></a>
+        <header>
+            <h1><?= $projectDetails["title"] ?></h1>
+        </header>
 
-        <a href="project_events.php?id=<?= $projectId ?>"><h3>Events</h3></a>
-        
-        <a href="project_sprints.php?id=<?= $projectId ?>"><h3>Sprints</h3></a>
+        <main>
+            <p><?= $projectDetails["description"] ?></p>
+            
+            <section>
+                <h2>Tasks</h2>
+                <a href="project_tasks.php?id=<?= $projectId ?>"><h3>Tasks</h3></a>
+            </section>
+            
+            <section>
+                <h2>Events</h2>
+                <a href="project_events.php?id=<?= $projectId ?>"><h3>Events</h3></a>
+            </section>
+            
+            <section>
+                <a href="project_sprints.php?id=<?= $projectId ?>"><h3>Sprints</h3></a>
+            </section>
 
-
-        <a class="logout-button" href="logout.php">Logout</a>
+            <a class="logout-button" href="logout.php">Logout</a>
+        </main>
     </BODY>
 
 </HTML>
