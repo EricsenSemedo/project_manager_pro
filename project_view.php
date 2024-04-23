8632557f7cc58a393a8fe981ebe12297691a5a24
EricsenSemedo <ericsen_semedo@uri.edu>
2024-04-23 01:20:33 -0400

    updated

--- project_view.php
+++ project_view.php
@@ -10,7 +10,7 @@
         exit;
     }
     
-    $projectId = $_GET["id"];
+    $project_id = $_GET["id"];
 
     //check if the user is a member of the project
     
@@ -18,37 +18,38 @@
     //get the user_id from the session 
     $user_id = $_SESSION["user_id"];
 
-    function getProjectDetails(PDO $pdo, int $projectId){
+    function getProjectDetails(PDO $pdo, int $project_id){
         $sql = "SELECT title, description
             FROM Projects
             WHERE project_id = :project_id;";
-        $project = pdo($pdo, $sql, ["project_id" => $projectId])->fetch();
+        $project = pdo($pdo, $sql, ["project_id" => $project_id])->fetch();
 
         return $project;
     }
 
-    function getAssociatedTasks(PDO $pdo, int $projectId){
+    function getAssociatedTasks(PDO $pdo, int $project_id){
         $sql = "SELECT task_id, title, description, due_date
-            FROM Tasks
+            FROM Task
             WHERE project_id = :project_id;";
-        $tasks = pdo($pdo, $sql, ["project_id" => $projectId])->fetchAll();
+        $tasks = pdo($pdo, $sql, ["project_id" => $project_id])->fetchAll();
 
         return $tasks;
     }
 
-    $projectDetails = getProjectDetails($pdo, $projectId);
+    $projectDetails = getProjectDetails($pdo, $project_id);
     $errorMessage = "";
 
-    getProjectEvents(PDO $pdo, int $project_id) {
+    function getProjectEvents(PDO $pdo, int $project_id) {
         $sql = "SELECT *
             FROM Event
-            WHERE project_id = :project_id;"
+            WHERE project_id = :project_id;";
         $events = pdo($pdo, $sql, ["project_id" => $project_id])->fetch();
 
         return $events;
     }
 
     $events = getProjectEvents($pdo, $project_id);
+    $associatedTasks = getAssociatedTasks($pdo, $project_id);
 ?>
 
 <!DOCTYPE>
@@ -58,21 +59,33 @@
         <link rel="stylesheet" href="css/style.css">
     </HEAD>
     <BODY>
-
-        <h1><?= $projectDetails["title"] ?></h1>
-        <p><?= $projectDetails["description"] ?></p>
-        
-        <a href="project_tasks.php?id=<?= $projectId ?>"><h3>Tasks</h3></a>
-
-        <a href="project_events.php?id=<?= $projectId ?>"><h3>Events</h3></a>
-        
-        <a href="project_sprints.php?id=<?= $projectId ?>"><h3>Sprints</h3></a>
-
-
-        <div class="topBar">
+        <header>
+            <h1><?php echo $projectDetails["title"]; ?></h1>
             <a class="logout-button" href="logout.php">Logout</a>
-            <h1 class="project-name"><?php echo "PROJ NAME HERE";?></h1>
-        </div>
+        </header>
 
+        <main>
+            <p><?= $projectDetails["description"] ?></p>
+            
+            <section class="khanban-board">
+                <ul class="khanban-column">
+                    <?php foreach ($associatedTasks as $task): ?>
+                        <li class="khanban-card">
+                            <h3><?= $task["title"] ?></h3>
+                            <p><?= $task["description"] ?></p>
+                        </li>
+                    <?php endforeach; ?>
+                    <a href="project_tasks.php?id=<?= $project_id ?>"><h3>Tasks</h3></a>
+                </ul>
+
+                <ul class="khanban-column">
+                    <a href="project_events.php?id=<?= $project_id ?>"><h3>Events</h3></a>
+                </ul>
+                
+                <ul class="khanban-column">
+                    <a href="project_sprints.php?id=<?= $project_id ?>"><h3>Sprints</h3></a>
+                </ul>
+            </section>
+        </main>
     </BODY>
 </HTML>
