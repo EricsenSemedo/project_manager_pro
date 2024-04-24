247fd916619a1c78ce6d0524c7b9236f770465d4
EricsenSemedo <ericsen_semedo@uri.edu>
2024-04-23 22:41:58 -0400

    removed userid requirement for form subission

--- register.php
+++ register.php
@@ -5,19 +5,17 @@
 
     $statusMessage = "";
 
-    function insertUser(PDO $pdo, string $firstname, string $lastname, string $email, string $userid, string $password){
+    function insertUser(PDO $pdo, string $firstname, string $lastname, string $email, string $password){
 
-        $sql = "INSERT INTO Users (user_id, first_name, last_name, password_hash, email) VALUES (:userid, :firstname, :lastname, :password, :email);";
+        $sql = "INSERT INTO Users (first_name, last_name, password_hash, email) VALUES (:firstname, :lastname, :password, :email);";
 
-        //$user = pdo($pdo, $sql, ["firstname" => $firstname, "lastname" => $lastname, "email" => $email, "userid" => $userid, "password" => $password])->fetch();
-        
         try {
             $stmt = $pdo->prepare($sql);
-            $stmt->execute(["firstname" => $firstname, "lastname" => $lastname, "email" => $email, "userid" => $userid, "password" => $password]);
+            $stmt->execute(["firstname" => $firstname, "lastname" => $lastname, "email" => $email, "password" => $password]);
             return true; // Return true if insertion is successful
         } catch (PDOException $e) {
             // Check if the error is due to duplicate user_id
-            if ($e->errorInfo[1] == 1062) {
+            if (isset($e->errorInfo[1]) && $e->errorInfo[1] == 1062) {
                 return false; // Return false if user_id already exists
             } else {
                 throw $e; // Re-throw the exception if it's not a duplicate user_id error
@@ -32,11 +30,10 @@
         $firstname = $_POST["firstname"];
         $lastname = $_POST["lastname"];
         $email = $_POST["email"];
-        $userid = $_POST["userid"];
         $password = $_POST["password"];
         // TODO: Hash the password before storing it in the variable to then compare it with the database
 
-        $userInserted = insertUser($pdo, $firstname, $lastname, $email, $userid, $password); 		// Get the user from the database
+        $userInserted = insertUser($pdo, $firstname, $lastname, $email, $password); 		// Get the user from the database
         
         if ($_POST["password"] != $_POST["password_check"]) {
             $statusMessage = "Passwords do not match.  Please try again.";
@@ -72,9 +69,6 @@
 
                 <label for="email">Email:</label>
                 <input type="text" name="email" id="email" required>
-
-                <label for="userid">Select a User ID:</label>
-                <input type="text" name="userid" id="userid" required>
 
                 <label for="password">Password:</label>
                 <input type="password" name="password" id="password" required>
